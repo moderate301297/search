85243a2d0730a483d63f8bdfd4761e7a83ec4861
Duongnd <duong.nd080297>
2019-09-18 12:36:20 +0700

    search field

--- search_box/js/load.js
+++ search_box/js/load.js
@@ -1,28 +1,77 @@
 $(document).ready(function () {
 
+    $.ajax({
+        type: "get",
+        url: "http://45.119.81.89:8090/rest/search/all",
+        success: function (res) {
+            html_count = '<p id="p-info-result">Khoảng ' + res.length + ' kết quả</p>'
+            document.getElementById('info-result').innerHTML = html_count
+
+            if (res.length === 0) {
+                var html = '<p id="p-info-result">không có kết quả thỏa mãn</p>'
+                document.getElementById('list-result').innerHTML = html
+
+            } else {
+                pagtination(res, 1);
+                loadSearch(res, 1)
+                loadDetail(res)
+            }
+        }
+    })
+
+    var arrDetail = ['content', 'name']
+    var arrNumber = [1, 1, 1]
+
+    var check_search = 0;
+
+    arrDetail.forEach(function (element, i) {
+
+        $('#input-' + arrDetail[i]).change(function () {
+            arrNumber.splice(i, 1, this.value);
+            document.getElementById(arrDetail[i] + '-number').innerHTML = this.value
+        })
+    })
+    var value_check_search = 1;
+
+    $('#checkbox-search').click(function () {
+
+        console.log(this.value)
+        if (this.value == 1) {
+            $('#checkbox-search').attr('value', 2)
+            check = 2
+        } else {
+            $('#checkbox-search').attr('value', 1)
+            check = 1
+        }
+
+        if (check == 2) {
+            $('.check-li').show()
+        } else {
+            $('.check-li').hide()
+        }
+
+    })
+
+
+
+
     var type_search = "content";
     // $.ajaxSetup({ cache: false });
 
     $('.type-search').click(function () {
-        $('#smart-search').attr('placeholder', 'Tìm kiếm theo ' + this.name)
+        $('#smart-search-content').attr('placeholder', 'Tìm kiếm theo ' + this.name)
         type_search = this.name
 
     })
 
-    $('#smart-search').click(function () {
-
-    })
-
-
-
-    $('#smart-search').keyup(function () {
+    $('#smart-search-content').keyup(function () {
         $('#dropdown').hide()
         $('#submit').css('margin-left', '10px')
         $('#upload-file').css('margin-left', '39px')
-        $('#smart-search').css('margin-left', '0px')
+        $('#smart-search-content').css('margin-left', '0px')
         $('#result').html('');
         $('#live-search').val('');
-        var searchField = $('#smart-search').val();
+        var searchField = $('#smart-search-content').val();
         var expression = new RegExp(searchField, "i");
 
         if (expression == '/(?:)/i') {
@@ -31,9 +80,9 @@ $(document).ready(function () {
             $.ajax({
                 type: "POST",
                 contentType: "application/json",
-                data: JSON.stringify({ "mgs": $('#smart-search').val() }),
+                data: JSON.stringify({ "mgs": $('#smart-search-content').val() }),
                 dataType: 'json',
-                cache: false,
+                // cache: false,
                 url: "http://45.119.81.89:5002/related",
                 success: function (res) {
                     document.getElementById('live-search').innerHTML = ''
@@ -50,7 +99,7 @@ $(document).ready(function () {
 
         $('#live-search').on('click', 'li', function () {
             var click_text = $(this).text().split('|');
-            $('#smart-search').val($.trim(click_text[0]));
+            $('#smart-search-content').val($.trim(click_text[0]));
             $("#live-search").html('');
             $('#related').show()
 
@@ -65,7 +114,7 @@ $(document).ready(function () {
         $('#dropdown').show()
         $('#submit').css('margin-left', '9px')
         $('#upload-file').css('margin-left', '10px')
-        // $('#smart-search').css('margin-left', '5px')
+        // $('#smart-search-content').css('margin-left', '5px')
         $(document).ready(function () {
             if ($('#div-dropdown').attr('class') === 'dropdown') {
                 $('#result-box').css('border-radius', '40px')
@@ -78,30 +127,69 @@ $(document).ready(function () {
 
     })
 
-    $('#smart-search').keypress(function (event) {
+    $('#smart-search-content').keypress(function (event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if (keycode == '13') {
-            smartSearch(type_search, $('#smart-search').val())
             $('#related').show()
-            loadRelated($('#smart-search').val())
+            loadRelated($('#smart-search-content').val())
+
+            if ($('#checkbox-search').val() == 1) {
+                smartSearchDocument($('#smart-search-content').val())
+            } else {
+                smartSearch($('#smart-search-content').val())
+
+            }
         }
     });
 
     $('#submit').click(function () {
-        smartSearch(type_search, $('#smart-search').val())
+        smartSearch(type_search, $('#smart-search-content').val())
         $('#related').show()
-        loadRelated($('#smart-search').val())
+
+        if ($('#checkbox-search').val() == 2) {
+            smartSearchDocument($('#smart-search-content').val())
+        } else {
+            smartSearch($('#smart-search-content').val())
+
+        }
 
     })
 
-    function smartSearch(type, text) {
+    function smartSearch(text) {
         document.getElementById('list-result').innerHTML = ''
         var key_search = $("#smart-search").val();
         if (key_search == '') {
         } else {
             $.ajax({
                 type: "get",
-                url: "http://45.119.81.89:8090/rest/search/" + type + "/" + text,
+                url: "http://45.119.81.89:8090/rest/search/field" + "/" + text,
+                success: function (res) {
+                    html_count = '<p id="p-info-result">Khoảng ' + res.length + ' kết quả</p>'
+                    document.getElementById('info-result').innerHTML = html_count
+
+                    if (res.length === 0) {
+                        var html = '<p id="p-info-result">không có kết quả thỏa mãn</p>'
+                        document.getElementById('list-result').innerHTML = html
+
+                    } else {
+                        pagtination(res, 1);
+                        loadSearchField(res, 1)
+                        loadDetailField(res)
+                    }
+                }
+            })
+
+        }
+    }
+
+    function smartSearchDocument(text) {
+        document.getElementById('list-result').innerHTML = ''
+        var key_search = $("#smart-search").val();
+        if (key_search == '') {
+        } else {
+            $.ajax({
+                type: "get",
+                url: "http://45.119.81.89:8090/rest/search/raw/content/" + text,
                 success: function (res) {
                     html_count = '<p id="p-info-result">Khoảng ' + res.length + ' kết quả</p>'
                     document.getElementById('info-result').innerHTML = html_count
@@ -165,6 +253,7 @@ $(document).ready(function () {
     }
 
     function pagtination(res, value) {
+
         var htmlPagtination = ""
         htmlPagtination += '<a href="#section" class="a-pagination" id="pagination-left" name=1>&laquo;</a>'
         var count = 0;
@@ -196,7 +285,6 @@ $(document).ready(function () {
         } else {
             jsonLength = parseInt(res.length / 10)
         }
-        console.log(value)
         if (parseInt(value) == 1) {
             $("#pagination-left").attr("name", 1);
             $('#pagination-right').attr("name", 2);
@@ -243,7 +331,7 @@ $(document).ready(function () {
         $.ajax({
             type: "POST",
             contentType: "application/json",
-            data: JSON.stringify({ "mgs": $('#smart-search').val() }),
+            data: JSON.stringify({ "mgs": $('#smart-search-content').val() }),
             dataType: 'json',
             cache: false,
             url: "http://45.119.81.89:5002/related",
@@ -264,42 +352,42 @@ $(document).ready(function () {
                 document.getElementById('related').innerHTML = html
 
                 $('.related-search').click(function () {
-                    $('#smart-search').val(this.text)
+                    $('#smart-search-content').val(this.text)
                     smartSearch(type_search, this.text)
                 })
             },
             error: function (e) {
                 console.log('error      ' + e)
             }
-
-
         })
-
-
-
-
     }
 
 
     $('#upload-file').click(function () {
         $('#result-box').hide()
         $('#smart-search-file').show()
-
     })
 
-    var arr = '';
+    $('#logo-search').click(function () {
+        $('#result-box').show()
+        $('#smart-search-file').hide()
+    })
+
     var file_search
-    // var file
     $('#input-smart-search-file').change(function (e) {
         $('#show-content').hide()
         $('#p-info-result').hide()
         $('#displayCheck').show()
         $('#select-box').show()
         file_search = this.files[0]
-        // file = e.target.files[0];
-        // var reader = new FileReader();
-        // var csv = reader.result.split(',');
-        // reader.readAsText(file)
+    })
+
+    var arrDetailFile = ['sel1', 'sel2', 'sel3', 'sel4', 'sel5', 'sel6', 'sel7', 'sel8', 'sel9', 'sel10', 'sel11']
+
+    arrDetailFile.forEach(function (element, i) {
+        $('#' + arrDetailFile[i]).change(function () {
+            document.getElementById(arrDetailFile[i] + '-number').innerHTML = this.value
+        })
     })
 
     $('#submit-search-file').click(function () {
@@ -308,7 +396,8 @@ $(document).ready(function () {
         $('#result-box').show()
         $('#smart-search-file').hide()
         $('#show-content').show()
-        arr = '';
+        var arr = ''
+
         for (i = 1; i < 12; i++) {
             arr += $('#sel' + i).val()
             if (i != 11) {
@@ -330,7 +419,7 @@ $(document).ready(function () {
             contentType: false,
             // dataType: 'json',
             cache: false,
-            url: "http://45.119.81.89:5000/",
+            url: "http://7d67e7a1.ngrok.io",
             success: function (res) {
                 html_count = '<p id="p-info-result">Khoảng ' + res.length + ' kết quả</p>'
                 document.getElementById('info-result').innerHTML = html_count
@@ -338,6 +427,9 @@ $(document).ready(function () {
                 if (res.length === 0) {
                     var html = '<p id="p-info-result">không có kết quả thỏa mãn</p>'
                     document.getElementById('list-result').innerHTML = html
+                } else if (res === 'Sorry, no matching files found') {
+                    var html = '<p id="p-info-result">không có kết quả thỏa mãn</p>'
+                    document.getElementById('list-result').innerHTML = 'Sorry, no matching files found'
                 } else {
                     pagtination(res, 1);
                     loadSearch(res, 1)
@@ -357,4 +449,48 @@ $(document).ready(function () {
     $('#smart-search-file').change(function (e) {
         $("#text-check").show()
     })
+
+    function loadSearchField(res, value) {
+        var html = ''
+        html += '<div class="scrollbar" id="style-7">'
+        html += '<div class="force-overflow">'
+        value = value * 10 - 10
+        if (value + 10 < res.length) {
+            for (i = value; i < value + 10; i++) {
+                html += '<div id="item' + i + '" class="info-search">'
+                html += '<div id="title-result" ><a href="#">' + res[i].nhanXet + '</a></div>'
+                html += '<div class="head-result" ><p>' + res[i].ten + '</p></div>'
+                html += '<div class="space-result"><br></div></div>'
+            }
+        } else {
+            for (i = value; i < res.length; i++) {
+
+                html += '<div id="item' + i + '" class="info-search">'
+                html += '<div id="title-result" ><a href="#">' + res[i].nhanXet + '</a></div>'
+                html += '<div class="head-result" ><p>' + res[i].ten + '</p></div>'
+                html += '<div class="space-result"><br></div></div>'
+            }
+        }
+        html += '</div>'
+        html += '</div>'
+        document.getElementById('list-result').innerHTML = html
+
+    }
+
+    function loadDetailField(res) {
+        res.forEach(function (element, i) {
+            $('#item' + i).click(function () {
+                html = ''
+                html += '<div class="scrollbar" id="style-7">'
+                html += '<div class="force-overflow">'
+
+                html += '<p class="pre-content-detail">' + res[i].nhanXet + '</p>'
+
+                html += '</div></div>'
+                document.getElementById('content-detail').innerHTML = html
+                // document.getElementById('detail-content').innerHTML = res[i].description
+            })
+        })
+    }
+
 })
